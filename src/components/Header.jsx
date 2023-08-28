f20aa237f712f7ea1608e2192b4201ffcefa96be
yoosemry <mcyoose@gmail.com>
2023-08-28 10:31:10 +0300

    header ..

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full bg-slate-50  rounded-t-lg p-4 text-black flex justify-between'>
+    <div className='w-full bg-slate-50  rounded-t-lg p-4 text-black flex justify-between items-center'>
 
     <div >
       <img src="https://forkify-v2.netlify.app/logo.09084f39.png" alt="logo" className='h-[3rem] object-cover block cursor-pointer' />
      
     </div>
-    <div className='bg-white px-4 py-2 relative flex justify-center text-center rounded-full'><input type="text" placeholder='search' className='px-4 w-[350px] outline-none text-xl' /> 
-    <button className='bg-yellow-400 absolute top-0 right-0 h-full  text-white py-2 px-8 text-xl rounded-full'>search</button>
+
+    <div className='bg-white px-4 py-2 relative flex justify-center items-center rounded-full'>
+    <input type="text" placeholder='search' className='px-4 w-[350px] outline-none text-xl' /> 
+    <button className='bg-yellow-400 absolute top-0 right-0 h-full flex justify-center items-center  text-white py-2 px-8 text-xl rounded-full'>search</button>
     
     </div>
+
+    
     <div>bookmark</div>
 
 
